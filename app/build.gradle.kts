2d63709906274d39c994286e2c34060f09df9471
TheoSfak <theodore.sfakianakis@gmail.com>
2026-04-26 14:32:02 +0300

    Polish app screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.geardex.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.5.4"
+        versionCode = 10
+        versionName = "1.5.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
